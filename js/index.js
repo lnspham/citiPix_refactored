ef41dd31943e0bf3c7526a87e03ddbaa219e328a
lnspham <lnspham@gmail.com>
2015-11-09 11:31:14 -0800

    Issue with working on attr(_) & toLowerCase()

--- js/index.js
+++ js/index.js
@@ -1,54 +1,59 @@
 $(document).ready(function(){
 
-//Prevent form submission
-event.preventDefault();
+	//Prevent form submission
+	event.preventDefault();
 
-//Define the variables
-var city = ["NYC", "SF", "LA", "ATX", "SYD"];
+	//Define the variables
+	var city = ["NYC", "SF", "LA", "ATX", "SYD"];
 
-for (i = 0; i < city.length; i++) { //Easy way to loop things. Way to keep doing things over and over again
-    //User make the selection provided by the <option>.  Iteration on the drop-down menu
-    $('select').append("<option>" + city[i]  + "</option>");  
-};
+	for (i = 0; i < city.length; i++) { //Easy way to loop things. Way to keep doing things over and over again
+	    //User make the selection provided by the <option>.  Iteration on the drop-down menu
+	    $('select').append("<option>" + city[i]  + "</option>");  
+	};
 
 
-//Capture user actions
-$('select').change(function(){
- 	var optionSelect = $('select').val();//get user selection
- 	var citySelected = optionSelect.toLowerCase();//change to lower case
+	//Capture user actions. Anonymous function
+	$('select').change(function(){
+	 	var optionSelect = $('select').val();//get user selection
+	//var citySelected = optionSelect.toLowerCase();//change to lower case
+	 	var switchBg =$('this').attr('class');//add a class to the <body> tag
 
- 	//if condition for the background images
- 	//addbg =$('this').attr('class')
- 	//check the light switch examples.
 
- 	if (citySelected == "NYC"){
+	 	$('body').attr('class', switchBg);
 
-			$('body').addClass('nyc');
-
-		 }else if (citySelected  == "SF"){
-
-		 	$('body').addClass('sf');
-
-		 }else if (citySelected  == "LA"){
-
-		 	$('body').addClass('la');
-		
-		 }else if (citySelected  == "ATX"){
-
-		 	$('body').addClass('austin');
-
-		 }else if (citySelected  == "SYD"){
-
-		 	$('body').addClass('sydney');
-
-		 }else{
-
-		 	$('body').addClass('citipix-skyline');
-		 }
-});
+	});
 
 
 });
 
 
 //array.forEach(function(elements, index){}0;
+
+//if condition for the background images
+// if (citySelected == "NYC"){
+
+		// 	$('body').attr('class');
+
+		//  }else if (citySelected  == "SF"){
+
+		//  	$('body').addClass('sf');
+
+		//  }else if (citySelected  == "LA"){
+
+		//  	$('body').addClass('la');
+		
+		//  }else if (citySelected  == "ATX"){
+
+		//  	$('body').addClass('austin');
+
+		//  }else if (citySelected  == "SYD"){
+
+		//  	$('body').addClass('sydney');
+
+		//  }else{
+
+		//  	$('body').addClass('citipix-skyline');
+	//  }
+
+
+
